a6366c3e711a7a4434d219a4bfba06263ac391ae
lokeshtemgire <160483794+lokeshtemgire@users.noreply.github.com>
2024-02-27 09:09:52 +0530

    Add files via upload

---
+++ python day 3.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a="sachin tendulkar"
+print(a(0))
+
+
+# In[2]:
+
+
+print(a[0])
+
+
+# In[4]:
+
+
+print(a[5])
+
+
+# In[5]:
+
+
+print(a[0:5])
+
+
+# In[6]:
+
+
+print(a[0:10])
+
+
+# In[7]:
+
+
+print(a[5:10])
+
+
+# In[12]:
+
+
+|## start :end: interval
+a="sachin Tendulkar"
+Print(a[: ])
+
+
+# In[13]:
+
+
+print(a[::1])
+
+
+# In[18]:
+
+
+print(a[::2])
+
+
+# In[19]:
+
+
+print(a[::3])
+
+
+# In[20]:
+
+
+a="sachin tendulkar"
+print(a[14])
+
+
+# In[21]:
+
+
+a="dhoni"
+print(a[14])
+
+
+# In[22]:
+
+
+print(a[-1])
+
+
+# In[23]:
+
+
+a="sachintendulkar"
+print(a[-1])
+
+
+# In[24]:
+
+
+print(a[::-1])
+
+
+# In[26]:
+
+
+## 
+a="Sachin Tendulkar"
+print(a.index("T"))
+
+
+# In[27]:
+
+
+print(a.index("a"))
+print(a.index(" "))
+print(a.index("a",2))
+print(a.index("a",2,15))
+
+
+# In[29]:
+
+
+print(a.rindex("T"))
+
+
+# In[30]:
+
+
+print(a.rindex("a"))
+
+
+# In[31]:
+
+
+a="virat i Kohli"
+print(a.index("i"))
+print(a.index("i",2))
+print(a.index("i",11))
+
+
+# In[34]:
+
+
+print(a.rindex("i"))
+
+
+# In[35]:
+
+
+## 
+a="sachin Tendulkar"
+print(a.index("z"))
+
+
+# In[36]:
+
+
+print(a.find("z")) ## -1 means false
+
+
+# In[38]:
+
+
+print(a.index("n"))
+print(a.find("n"))
+
+
+# In[39]:
+
+
+print(a.index("T"))
+print(a.find("T"))
+
+
+# In[41]:
+
+
+## Strip (remove Spaces)
+a=" hi hello "
+print(a.strip())
+print(a.lstrip())
+print(a.rstrip())
+
+
+# In[42]:
+
+
+#
+a="hi hello welcome"
+print(a.replace(" ",""))
+
+
+# In[47]:
+
+
+Assignment
+#a="dhoni"
+## get last letter i (without using reverse index)
+
+a="dhoni"
+print(a[4])
+
+
+# In[48]:
+
+
+a="dhoni"
+print(a[4])
+
+
+# In[ ]:
+
+
+
+
